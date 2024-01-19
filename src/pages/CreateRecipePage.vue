52658221e21e88715dcef255fbfbdc440fe5e4b2
kathima <k.machilek@gmx.de>
2024-01-19 16:15:26 +0100

    add new input field

--- src/pages/CreateRecipePage.vue
+++ src/pages/CreateRecipePage.vue
@@ -2,14 +2,28 @@
 import {ref} from "vue";
 
 const inputTitle = ref("");
-const quantity = ref("");
+const ingredients = ref([{
+  quantity: "",
+  measurement: "Gramm",
+  ingredient: "",
+
+}])
+const ingredientsData = ref({
+  quantity: "",
+      measurement: "Gramm",
+    ingredient: "",
+
+})
+/*const quantity = ref("");
 const measurement = ref("Gramm")
-const ingredient = ref("");
+const ingredient = ref("");*/
+const description = ref("");
 
-
+function addInputField() {
+  ingredients.value.push(ingredientsData.value)
+}
 function saveInput() {
-  console.log(inputTitle.value)
-  console.log(ingredient.value)
+  console.log(ingredients.value)
 }
 
 
@@ -21,21 +35,30 @@ function saveInput() {
       <p>Rezept Name:</p>
       <input v-model="inputTitle" required=true id="title">
       <p>Zutaten:</p>
-      <div class="ingredients">
-        <input v-model="quantity" type="number" placeholder="Menge" class="quantity" required=true id="quantity" >
-        <select v-model="measurement">
-          <option>Gramm</option>
-          <option>Kilogramm</option>
-          <option>Milliliter</option>
-          <option>Liter</option>
-          <option>Esslöffel</option>
-          <option>Teelöffel</option>
-        </select>
-        <input v-model="ingredient" placeholder="Zutat" class="ingredientInput" required=true id="ingredient">
-        <button class="deleteIngredient" >-</button>
-      </div>
+        <div v-for="ingredient in ingredients">
+          <div class="ingredients" key="index">
+            <input v-model="ingredient.quantity" type="number" placeholder="Menge" class="quantity" required=true
+                   id="quantity">
+            <select v-model="ingredient.measurement">
+              <option>Gramm</option>
+              <option>Kilogramm</option>
+              <option>Milliliter</option>
+              <option>Liter</option>
+              <option>Esslöffel</option>
+              <option>Teelöffel</option>
+            </select>
+            <input v-model="ingredient.ingredient" placeholder="Zutat" class="ingredientInput" required=true
+                   id="ingredient">
+            <button class="deleteIngredient">-</button>
+          </div>
+        </div>
+
       <div/>
-      <button>+</button>
+      <button  @click="addInputField">+ (mehr Zutaten)</button>
+      <p>Beschreibung:</p>
+      <textarea v-model="description"></textarea>
+      <p>Bilder:</p>
+      <p>TODO: Bild Upload</p>
     </form>
     <button @click="saveInput">Speichern</button>
     <button @click="saveInput">Abbrechen</button>
